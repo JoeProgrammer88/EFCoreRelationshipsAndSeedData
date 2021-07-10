0ae4e2647cd562b9accba9da3684f0a616ca3741
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-07-09 17:01:48 -0700

    Remove unused code, add clarifying comments

--- EFCoreRelationshipsAndSeedData/Models/MyDbContext.cs
+++ EFCoreRelationshipsAndSeedData/Models/MyDbContext.cs
@@ -29,58 +29,16 @@ namespace EFCoreRelationshipsAndSeedData.Models
                     new Category { CategoryId = 2, CategoryName = "Home" }
                 );
 
+            // Have to manually add rows to linking/junction table
             builder.Entity<Product>()
                 .HasMany(p => p.Categories)
                 .WithMany(cat => cat.Products)
                 .UsingEntity(junctionTable => junctionTable.HasData(
+                    // Notice these are anonymous types because the linking table
+                    // is not definited in the DBContext
                     new { CategoriesCategoryId = 1, ProductsProductId = 1 },
                     new { CategoriesCategoryId = 2, ProductsProductId = 1 }
                 ));
-
-            //builder.Entity<Product>()
-            //    .HasMany(p => p.Categories)
-            //    .WithMany(cat => cat.Products)
-            //    .UsingEntity(prodCats => prodCats.ToTable("CategoryProduct")); // Hidden linking table
-
-            //Product prod1 = new Product()
-            //{
-            //    ProductId = 1,
-            //    Price = 9.99,
-            //    ProductName = "test"
-            //};
-
-            //List<Product> products = new List<Product>() { prod1 };
-
-            //builder.Entity<Product>(p =>
-            //{
-            //    p.HasData(prod1);
-            //    p.OwnsMany(p => p.Categories).HasData(new
-            //    {
-            //        CategoryId = 1, CategoryName = "Electronic", ProductId = 1
-            //    });
-            //});
-            //builder.Entity<Product>()
-            //    .OwnsOne(p => p.Categories);
-
-            //builder.Entity<Category>()
-            //    .OwnsOne(cat => cat.Products);
-
-            //Category cat1 = new Category { CategoryId = 1, CategoryName = "Electronics" };
-            //Category cat2 = new Category { CategoryId = 2, CategoryName = "Automotive" };
-            //Category cat3 = new Category { CategoryId = 3, CategoryName = "Snacks" };
-            //Category cat4 = new Category { CategoryId = 4, CategoryName = "Home" };
-
-            //Product prod1 = new Product
-            //{
-            //    ProductId = 1,
-            //    ProductName = "Test Product"
-            //};
-
-            //builder.Entity<Category>().HasData(cat1, cat2, cat3, cat4);
-
-            //builder.Entity<Product>().HasData(prod1);
-            //builder.Entity<Product>().HasData(prod1.Categories = new List<Category>() { cat1, cat2 });
-
         }
     }
 }
